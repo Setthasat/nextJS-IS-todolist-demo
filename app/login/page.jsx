3812060b0f0c0f6def24de5997e7c68f4841c9a1
Setthasat Sirikomonsing <setthasatsirikomonsing@Setthasats-Mac-mini.local>
2024-12-01 21:44:14 +0700

    ui fixed

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <div className="bg-gradient-to-b from-[#FFB114] to-[#FF4FCA] w-screen h-screen">
-            <h1>Login Page</h1>
-            <Link href="/">
-                <a>Go to Home</a>
-            </Link>
+        <div className="bg-gradient-to-b from-[#FFB114] to-[#FF4FCA] w-screen h-screen flex justify-center items-center">
+            <div className="h-full w-full  flex flex-col justify-center items-center">
+                <h1 className="text-white text-3xl">Comming soon ...</h1>
+                <Link href="/" className="text-blue-500 text-2xl underline underline-offset-8">
+                    Go to Home
+                </Link>
+            </div>
         </div>
     );
 }
