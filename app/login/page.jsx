9767623ae2164edb252f6fb7a9f95154feef859d
Setthasat Sirikomonsing <setthasatsirikomonsing@Setthasats-Mac-mini.local>
2024-12-01 19:12:33 +0700

    ui fixed

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <div>
+        <div className="bg-gradient-to-b from-[#FFB114] to-[#FF4FCA] w-screen h-screen">
             <h1>Login Page</h1>
             <Link href="/">
                 <a>Go to Home</a>
